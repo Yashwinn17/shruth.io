fc8806849eb257caf0ba06bf4f39639103e47f76
Yashwinn17 <136632653+Yashwinn17@users.noreply.github.com>
2023-06-16 00:51:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -54,7 +54,7 @@ ScrollReveal().reveal('.home-content p, .about-content',{ origin: 'right'});
 
 //============================  types js =============
 const typed = new Typed('.multiple-text',{
-    strings: ['Frontend Developer', 'Android Developer', 'Tech Enthusiast', 'Entrepreneur', 'IITian', 'Learner'],
+    strings: ['Data Analytics Enthusiast', 'Software Engineer', 'Graduate Student'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
